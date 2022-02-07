7e917e491fb82d81d680e4dbdd934c1d7d83393f
hxie0 <xhy@chicagobooth.edu>
2022-02-07 22:58:43 +0000

    test6

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import requests
 import pickle
 import numpy as np
 import sklearn
-
 from sklearn.preprocessing import StandardScaler
+
 app = Flask(__name__)
 model = pickle.load(open('final_model.pkl','rb'))
 
