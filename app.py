616a5f903f71e6a72804b0945208595bc45a5b97
hxie0 <xhy@chicagobooth.edu>
2022-02-08 04:49:02 +0000

    test4.0

--- app.py
+++ app.py
@@ -7,4 +7,46 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-    
+@app.route('/predict', methods = ['POST'])
+def predict():
+    Fuel_Type_Diesel =0
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        Present_Price = float(request.form['Present_Price'])
+        Kms_Driven = int(request.form['Kms_Driven'])
+        Owner = int(request.form['Owner'])
+        Fuel_Type_Petrol = request.form['Fuel_Type_Petrol']
+        if(Fuel_Type_Petrol == 'Petrol'):
+            Fuel_Type_Diesel = 0
+            Fuel_Type_Petrol = 1
+
+        elif(Fuel_Type_Diesel=='Diesel'):
+            Fuel_Type_Petrol = 0
+            Fuel_Type_Diesel = 1
+        else:
+            Fuel_Type_Petrol = 0
+            Fuel_Type_Diesel = 0
+
+        Year = 2020 - Year
+        Seller_Type_Individual = request.form['Seller_Type_Individual']
+        if(Seller_Type_Individual=='Individual'):
+            Seller_Type_Individual =1
+        else:
+            Seller_Type_Individual = 0
+
+        Transmission_Manual = request.form['Transmission_Manual']
+        if(Transmission_Manual == 'Manual'):
+            Transmission_Manual = 1
+        else:
+            Transmission_Manual = 0
+
+        prediction = model.predict([[Present_Price,Kms_Driven,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Manual]])
+        output = round(prediction[0],2)
+
+        if output<0:
+            return render_template('index.html',prediction_text='This car has no remaining value and should be retired.')
+        else:
+            return render_template('index.html', prediction_text='The fair market value for this car is {} dollars.'.format(output))
+
+    else:
+        return render_template('index.html')
