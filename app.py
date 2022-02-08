e911ab1683f571831fd8f0f108342eb79265dbea
hxie0 <xhy@chicagobooth.edu>
2022-02-08 06:31:27 +0000

    Try1

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+#import jsonify
+#import requests
+#import pickle
+#import numpy as np
+#import sklearn
+#from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
+#model = pickle.load(open('final_model.pkl','rb'))
 
 @app.route('/')
 def index():
@@ -9,44 +16,19 @@ def index():
 
 @app.route('/predict', methods = ['POST'])
 def predict():
-    Fuel_Type_Diesel =0
     if request.method == 'POST':
         Year = int(request.form['Year'])
-        Present_Price = float(request.form['Present_Price'])
-        Kms_Driven = int(request.form['Kms_Driven'])
-        Owner = int(request.form['Owner'])
-        Fuel_Type_Petrol = request.form['Fuel_Type_Petrol']
-        if(Fuel_Type_Petrol == 'Petrol'):
-            Fuel_Type_Diesel = 0
-            Fuel_Type_Petrol = 1
-
-        elif(Fuel_Type_Diesel=='Diesel'):
-            Fuel_Type_Petrol = 0
-            Fuel_Type_Diesel = 1
-        else:
-            Fuel_Type_Petrol = 0
-            Fuel_Type_Diesel = 0
-
-        Year = 2020 - Year
-        Seller_Type_Individual = request.form['Seller_Type_Individual']
-        if(Seller_Type_Individual=='Individual'):
-            Seller_Type_Individual =1
-        else:
-            Seller_Type_Individual = 0
-
-        Transmission_Manual = request.form['Transmission_Manual']
-        if(Transmission_Manual == 'Manual'):
-            Transmission_Manual = 1
-        else:
-            Transmission_Manual = 0
-
-        prediction = model.predict([[Present_Price,Kms_Driven,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Manual]])
-        output = round(prediction[0],2)
-
+        Odometer = int(request.form['Odometer'])
+        Brand_Name = str(request.form['Brand_Name'])
+        Model_Name = str(request.form['Model_Name'])
+        Condition = int(request.form['Condition'])
+        
+        #prediction = model.predict([[Year,Odometer,Brand_Name,Model_Name,Condition]])
+        #output = round(prediction[0],2)
+        output = 35000
         if output<0:
             return render_template('index.html',prediction_text='This car has no remaining value and should be retired.')
         else:
-            return render_template('index.html', prediction_text='The fair market value for this car is {} dollars.'.format(output))
-
+            return render_template('index.html', prediction_text='The fair market value for {} {} {} is {} dollars.'.format(Year, Brand_Name, Model_Name, output))
     else:
         return render_template('index.html')
