88af3da0438462c01bd259d1b6fe504074065b13
hxie0 <xhy@chicagobooth.edu>
2022-02-08 02:22:22 +0000

    try3

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: python app.py
+heroku config:set BUILDPACK_URL=https://github.com/dbrgn/heroku-buildpack-python-sklearn/
--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask,render_template,request
 #import jsonify
 #import requests
-import pickle
+#import pickle
 #import numpy as np
-import sklearn
-from sklearn.preprocessing import StandardScaler
+#import sklearn
+#from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
-model = pickle.load(open('final_model.pkl','rb'))
+#model = pickle.load(open('final_model.pkl','rb'))
 
 @app.route('/', methods=['GET'])
 def Home():
   return render_template('index.html')
 
-standard_to = StandardScaler()
+#standard_to = StandardScaler()
 
 @app.route('/predict', methods = ['POST'])
 def predict():
